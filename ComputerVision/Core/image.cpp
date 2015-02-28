7d1e650a5c6130529f76c5d94aaba0745df6959b
Alina_Smirnova <alinasmirnova915@gmail.com>
2015-02-28 18:07:13 +0600

    base image read/write functions

--- ComputerVision/Core/image.cpp
+++ ComputerVision/Core/image.cpp
@@ -7,3 +7,91 @@ Image::Image(int h, int w)
     height = h;
     weight = w;
 }
+
+shared_ptr<Image> Image::fromFile(const char* fileName)
+{
+    QImage picture;
+    bool isLoaded = picture.load(fileName);
+    if(isLoaded)
+    {
+        qDebug() << "Load image from file: " << fileName;
+        return fromQImage(picture);
+    }
+    else
+    {
+        qWarning() << "Failed to load image from file";
+        return nullptr;
+    }
+}
+
+shared_ptr<Image> Image::fromQImage(QImage picture)
+{
+    shared_ptr<Image> result = make_shared<Image>(picture.height(), picture.width()());
+    QRgb original;
+    int color;
+
+    for(int i=0; i<picture.height(); i++)
+    {
+        for(int j=0; j<picture.width(); j++)
+        {
+            original = picture.pixel(j,i);
+            color = qRound(0.299 * qRed(original) +
+                           0.587 * qGreen(original) +
+                           0.114 * qBlue(original));
+            result->setPixel(i,j,color);
+        }
+    }
+
+    return result;
+}
+
+QImage toQImage()
+{
+    QImage result = QImage(width, height, QImage::Format_RGB32);
+    int color;
+    for(int i=0; i<height; i++)
+    {
+        for(int j=0; j<weight; j++)
+        {
+            color = getPixel(i, j);
+            result.setPixel(j, i, qRgb(color,color,color));
+        }
+    }
+    return result;
+}
+
+int getPixel(int i, int j)
+{
+    if(i<height && j<weight && i>0 && j>0)
+    {
+        return image[i*weight + j];
+    }
+    else
+    {
+        qFatal() << "Index out of the range: [" << i << "][" << j << "]";
+        return -1;
+    }
+}
+
+int setPixel(int i, int j, int value)
+{
+    if(i<height && j<weight && i>0 && j>0)
+    {
+        if(value < 0)
+        {
+            value = 0;
+        }
+        else if(value > 255)
+        {
+            value = 255;
+        }
+
+        image[i*weight + j] = value;
+        return value;
+    }
+    else
+    {
+        qFatal() << "Index out of the range: [" << i << "][" << j << "]";
+        return -1;
+    }
+}
--- ComputerVision/Core/image.h
+++ ComputerVision/Core/image.h
@@ -1,12 +1,22 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 #include <memory>
+#include <QImage>
+#include <QDebug>
 using namespace std;
 
 class Image
 {
-public:
     Image(int h, int w);
+
+public:
+    static shared_ptr<Image> fromFile(const char* fileName);
+    static shared_ptr<Image> fromQImage(QImage picture);
+
+    QImage toQImage();
+
+    int getPixel(int i, int j);
+    int setPixel(int i, int j, int value);
 
 private:
     unique_ptr<int[]> image;
